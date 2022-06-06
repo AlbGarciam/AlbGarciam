3fdfc1bfa6bb394b8d09bbb8629812d1974c4c97
Alberto Garcia <alb.garciam@gmail.com>
2022-06-06 20:24:46 +0200

    Support clicks

--- src/atoms/SectionCard/index.js
+++ src/atoms/SectionCard/index.js
@@ -3,10 +3,14 @@ import "../../css/typography.css";
 import "./styles.css";
 
 export default class SectionCard extends React.Component {
+  onClick(e) {
+    this.props.onClick();
+  }
+
   render() {
     const className = "section-card " + this.props.type;
     return (
-      <div className={className}>
+      <div className={className} onClick={this.onClick.bind(this)}>
         <div>
           <p className="text-semibold">{this.props.title}</p>
           <p className="text-semibold">{this.props.subtitle}</p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,17 +3,46 @@ import SectionCard from "../atoms/SectionCard";
 import TaglineCard from "../atoms/TaglineCard";
 import "./index.css";
 
-export default function IndexPage() {
-  return (
-    <main className="main">
-      <div className="top-group">
-        <TaglineCard type="about" title="About" subtitle="Me" />
-        <SectionCard type="about" title="About" subtitle="Me" />
-      </div>
-      <div className="bottom-group">
-        <SectionCard type="projects" title="My" subtitle="projects" />
-        <SectionCard type="contact" title="Contact" subtitle="Me" />
-      </div>
-    </main>
-  );
+export default class IndexPage extends React.Component {
+  onAboutClicked() {
+    console.log("About tapped");
+  }
+
+  onProjectsClicked() {
+    console.log("Project tapped");
+  }
+
+  onContactClicked() {
+    console.log("Contact tapped");
+  }
+
+  render() {
+    return (
+      <main className="main">
+        <div className="top-group">
+          <TaglineCard />
+          <SectionCard
+            type="about"
+            title="About"
+            subtitle="Me"
+            onClick={this.onAboutClicked.bind(this)}
+          />
+        </div>
+        <div className="bottom-group">
+          <SectionCard
+            type="projects"
+            title="My"
+            subtitle="projects"
+            onClick={this.onProjectsClicked.bind(this)}
+          />
+          <SectionCard
+            type="contact"
+            title="Contact"
+            subtitle="Me"
+            onClick={this.onContactClicked.bind(this)}
+          />
+        </div>
+      </main>
+    );
+  }
 }
