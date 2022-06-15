be531febbee8ae92e3338b832a8f49fa790f83c7
Alberto Garcia <alb.garciam@gmail.com>
2022-06-15 20:00:23 +0200

    Remove web

--- src/atoms/SectionTitle/index.js
+++
@@ -1,9 +0,0 @@
-import { navigate } from "gatsby";
-import React from "react";
-import "./style.css";
-
-export default class SectionTitle extends React.Component {
-  render() {
-    return <h1 className="section-title text-regular">{this.props.title}</h1>;
-  }
-}
